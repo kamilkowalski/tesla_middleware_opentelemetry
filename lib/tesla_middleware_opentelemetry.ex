966757e52e3ea40a6a816f7e293555d7cf0c2816
Kamil Kowalski <kamil@kamilkowalski.pl>
2021-09-07 22:10:36 +0200

    Initialize project

---
+++ lib/tesla_middleware_opentelemetry.ex
@@ -0,0 +1,18 @@
+defmodule TeslaMiddlewareOpentelemetry do
+  @moduledoc """
+  Documentation for `TeslaMiddlewareOpentelemetry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TeslaMiddlewareOpentelemetry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tesla_middleware_opentelemetry_test.exs
@@ -0,0 +1,8 @@
+defmodule TeslaMiddlewareOpentelemetryTest do
+  use ExUnit.Case
+  doctest TeslaMiddlewareOpentelemetry
+
+  test "greets the world" do
+    assert TeslaMiddlewareOpentelemetry.hello() == :world
+  end
+end
