2a6ed8737363c0a0cefed0b28d617aa45c6f914a
Kamil Kowalski <kamil@kamilkowalski.pl>
2021-09-07 23:51:39 +0200

    Implement the middleware

--- lib/tesla_middleware_opentelemetry.ex
+++ lib/tesla_middleware_opentelemetry.ex
@@ -1,18 +1,43 @@
 defmodule TeslaMiddlewareOpentelemetry do
+  @behaviour Tesla.Middleware
+
+  require OpenTelemetry.Tracer, as: Tracer
+
   @moduledoc """
-  Documentation for `TeslaMiddlewareOpentelemetry`.
+  Tesla middleware for propagating OpenTelemetry spans with requests.
+
+  This middleware will create a new span and include it in request
+  headers using the W3C Trace Context headers.
   """
 
-  @doc """
-  Hello world.
+  @impl Tesla.Middleware
+  def call(env, next, _opts) do
+    uri = %URI{path: path} = URI.parse(env.url)
 
-  ## Examples
+    Tracer.with_span path, %{attributes: http_attributes(env, uri)} do
+      ctx = Tracer.current_span_ctx()
+      headers = :otel_propagator_http_w3c.inject(ctx)
 
-      iex> TeslaMiddlewareOpentelemetry.hello()
-      :world
+      env
+      |> Tesla.put_headers(headers)
+      |> Tesla.run(next)
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defp http_attributes(env, uri) do
+    attributes = [
+      "http.host": uri.host,
+      "http.method": env.method,
+      "http.path": uri.path,
+      "http.url": env.url
+    ]
+
+    case Tesla.get_header(env, "user-agent") do
+      nil ->
+        attributes
+
+      user_agent ->
+        Keyword.put(attributes, :"http.user_agent", user_agent)
+    end
   end
 end
